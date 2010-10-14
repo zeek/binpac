1af528ddbb8f2ebedf4df183830e6aab347e5121
Jon Siwek <jsiwek@ncsa.illinois.edu>
2010-10-14 10:17:55 -0500

    Initial, working CMake build added.

---
+++ BuildOptions.cmake
@@ -0,0 +1,9 @@
+#--------------------------------------------------------------------#
+#                                                                    #
+#                          BinPAC - Build Setup                      #
+#                                                                    #
+#--------------------------------------------------------------------#
+#
+# The installation directory
+set(CMAKE_INSTALL_PREFIX /usr/local
+    CACHE STRING "Installation directory" FORCE)
