dea8ed19b498f78b99bccc9e1b0bca7faf7f51e5
Jon Siwek <jsiwek@ncsa.illinois.edu>
2010-10-20 13:17:59 -0500

    Added debug/release mode compiler flags and build summary output.

--- BuildOptions.cmake
+++ BuildOptions.cmake
@@ -7,3 +7,9 @@
 # The installation directory
 set(CMAKE_INSTALL_PREFIX /usr/local
     CACHE STRING "Installation directory" FORCE)
+
+set(ENABLE_DEBUG false
+    CACHE STRING "Compile with debugging symbols" FORCE)
+
+set(ENABLE_RELEASE false
+    CACHE STRING "Use -O3 compiler optimizations" FORCE)
