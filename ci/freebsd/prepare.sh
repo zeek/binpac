2373a39060f434bcedc89abc551e9e4ca4a922d1
Tim Wojtulewicz <tim@corelight.com>
2023-01-27 07:53:11 -0700

    Add config for Cirrus

---
+++ ci/freebsd/prepare.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Preparing FreeBSD environment"
+sysctl hw.model hw.machine hw.ncpu
+set -e
+set -x
+
+env ASSUME_ALWAYS_YES=YES pkg bootstrap
+pkg install -y bash git cmake bison base64 flex
+pkg upgrade -y curl
